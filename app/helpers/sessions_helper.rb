da5f6c61277bfae7abab08251ffb56dfa1b7b661
ninoko1995 <ninomiya.infratop@gmial.com>
2017-02-11 05:35:28 +0000

    finish login-logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,28 @@
 module SessionsHelper
+	 #渡されたユーザーでログインする
+  def log_in(user)
+  	session[:user_id] = user.id
+  end
+
+  def current_user
+  	# if @current_user.nil?
+  	# 	@current_user = User.find_by(id: session[:user_id])#findだと例外を返してしまうので、find_byを用いる
+  	# else
+  	# 	@current_user
+  	# end
+
+  	# @current_user = @current_user || User.find_by(id: session[:user_id])
+
+  	@current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+  	!current_user.nil?
+  end
+
+  #現在のユーザーのログアウト
+  def log_out
+  	session.delete(:user_id)
+  	@current_user = nil
+  end
 end
