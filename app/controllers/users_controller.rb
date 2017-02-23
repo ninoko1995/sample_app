a63cb56161b9883cafa6873e6fb153957a02e769
ninoko1995 <ninomiya.infratop@gmial.com>
2017-02-23 10:05:43 +0000

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:edit,:update]
+  before_action :logged_in_user, only:[:edit,:update,:index]
   before_action :correct_user, only:[:edit,:update]
   before_action :set_user, only: [:show,:edit,:update]
 
@@ -31,6 +31,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
