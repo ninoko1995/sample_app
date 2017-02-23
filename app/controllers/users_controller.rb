246321841a8b577af8aac54385ac8c47e789316d
ninoko1995 <ninomiya.infratop@gmial.com>
2017-02-23 08:33:34 +0000

    10-2-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only:[:edit,:update]
   before_action :set_user, only: [:show,:edit,:update]
+  
   def new
   	@user=User.new
   end
@@ -41,4 +43,12 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'please log in'
+        redirect_to login_url
+      end
+    end
+
 end
