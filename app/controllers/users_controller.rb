993da3bd3f5ec2df0913c334a182293de8813f58
ninoko1995 <ninomiya.infratop@gmial.com>
2017-02-23 08:16:10 +0000

    10-1-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update]
   def new
   	@user=User.new
   end
@@ -18,24 +19,26 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合を扱う。
+      flash[:success]='Successfully updated!'
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
   def show
-  	@user=User.find(params[:id])
   end
 
   private
 	  def user_params
 	  	params.require(:user).permit(:name, :email, :password,:passwoed_confirmation)
 	  end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
